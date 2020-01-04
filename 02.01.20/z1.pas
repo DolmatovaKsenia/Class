0d2117f1d66920f0ad546cf2463d90aae21fe322
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-01-04 21:10:25 +0300

    02.01.20
    Added classwork

---
+++ 02.01.20/z1.pas
@@ -0,0 +1,20 @@
+﻿// Дано вещественное число A и целое число N (> 0). Используя один цикл,
+//найти сумму
+//1 + A + A2 + A3 + … + AN.
+
+var
+  A, sum, powerA: real;
+  N: integer;
+  
+begin
+  readln(A, N);
+  powerA := 1;
+  sum := 1;
+  for var i := 1 to N do
+  begin
+    powerA := powerA * A;
+    sum := sum + powerA
+  end;
+  writeln(sum)
+  
+end.
---
+++ 02.01.20/z3.pas
@@ -0,0 +1,24 @@
+﻿// Дано вещественное число A и целое число N (> 0). Используя один цикл,
+//найти значение выражения
+// A - A2 + A3 - … + (–1)N+1·AN.
+//Условный оператор не использовать.
+
+
+var
+  A, sum, powerA: real;
+  N, sign: integer;
+  
+begin
+  readln(A, N);
+  powerA := 1;
+  sum := 1;
+  sign := -1;
+  for var i := 1 to N do
+  begin
+    sign := sign * (-1);
+    powerA := powerA * A;
+    sum := sum + powerA * sign
+  end;
+  writeln(sum)
+  
+end.
