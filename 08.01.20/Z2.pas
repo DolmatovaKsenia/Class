2754a74ae00cac6c2de76e78d6a62c59731c8874
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-01-08 20:23:54 +0300

    added classwork

---
+++ 08.01.20/Z2.pas
@@ -0,0 +1,20 @@
+﻿// Дано целое число N (> 1). Найти наименьшее целое число K, при кото-
+//ром выполняется неравенство 3^K > N.
+
+var
+  N, K, power3: integer;
+  
+begin
+  readln(N);
+  K := 1;
+  power3 := 3;
+  while power3 <= N do
+  begin
+   power3 := power3 * 3;
+   K := K + 1;
+  end;
+  writeln(K)
+   
+   
+    
+end.
---
+++ 08.01.20/Z3.pas
@@ -0,0 +1,21 @@
+﻿// Дано целое число N (> 0). Используя операции деления нацело и взя-
+//тия остатка от деления, найти количество и сумму его цифр.
+
+var
+  N, sum, count, number: integer;
+  
+begin
+  readln(N);
+  number := N;
+  count := 0;
+  sum := 0;
+  while number > 0 do
+  begin
+    count := count + 1;
+    sum := sum + number mod 10;
+    number := number div 10;
+    
+  end;
+  writeln(sum, ' ', count);
+end.
+  
