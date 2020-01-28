9d49f6f2654425eb4cfa51deafe9d986cf325361
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-01-28 18:35:43 +0300

    added classwork

---
+++ 22_01/Z1.pas
@@ -0,0 +1,22 @@
+﻿// Массив 15 целых случайных чисел. X(-10;10) найти и вывести сумму элементов оканчивающихся на 3.
+
+const
+  size = 15;
+
+var
+  arr: array [1..size] of integer;
+  sum: integer;
+  
+begin
+ sum := 0;
+ for var i := 1 to size do
+  begin
+    arr[i] := random(-10, 10);
+    if arr[i] mod 10 = 3 then
+      sum += arr[i];
+    
+    
+  end;
+  writeln(sum);
+  
+end.
---
+++ 22_01/Z3.pas
@@ -0,0 +1,50 @@
+﻿//Arr17
+
+const 
+  size = 10;
+
+var
+  arr: array[1..size] of integer;
+  indexDirect: integer;
+  indexReverse: integer;
+  indexPrinted: integer;
+  
+begin
+  indexDirect := 1;
+  indexReverse := size;
+  indexPrinted := 0;
+  
+  for var i := 1 to size do
+    arr[i] := random(-9, 9);
+    
+  for var i := 1 to size do
+    write(arr[i], ' ');
+    
+  writeln();
+  
+  while indexPrinted <> size do
+  begin
+    write(arr[indexDirect], ' ');
+    indexPrinted += 1;
+    indexDirect += 1;
+    
+    if size = indexPrinted then
+      break;
+    write(arr[indexDirect], ' ');
+    indexPrinted += 1;
+    indexDirect += 1;
+    
+    if size = indexPrinted then
+      break;
+    write(arr[indexReverse], ' ');
+    indexPrinted += 1;
+    indexReverse -= 1;
+    
+    if size = indexPrinted then 
+      break;
+    write(arr[indexReverse], ' ');
+    indexPrinted += 1;
+    indexReverse -= 1;
+  end;
+    
+end.
