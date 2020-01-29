1b875261ec7514a13ef0ad6d33045815e3237b55
Victoria <viky.fomina@yandex.ru>
2020-01-29 19:37:21 +0300

    an example

---
+++ 29_01/Program1.pas
@@ -0,0 +1,22 @@
+﻿// Дано целое число N и набор из N вещественных чисел. Вывести сумму
+// и произведение чисел из данного набора. 
+
+var
+  N: integer;
+  number, sum, product: real;
+  
+begin
+  readln(N);
+  
+  sum := 0;
+  product := 1;
+  
+  for var i := 1 to N do
+  begin
+    readln(number);
+    sum += number;
+    product *= number;
+  end;
+  
+  writeln('Sum is ', sum, '; product is ', product)
+end.
