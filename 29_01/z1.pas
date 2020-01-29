eba14328c26550deb6b38fb0aecb9015519eb992
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-01-30 00:11:46 +0300

    added classwork_29_01_20

---
+++ 29_01/z1.pas
@@ -0,0 +1,22 @@
+﻿// Дано целое число N и набор из N целых чисел. Вывести в том же по-
+// рядке все четные числа из данного набора и количество K таких чисел.
+
+var
+  N, number, K: integer;
+  
+begin
+  readln(N);
+  K := 0;
+  for var i := 1 to N do
+  begin
+    readln(number);
+    if number mod 2 = 0 then
+    begin
+      K += 1;
+      writeln(number);
+    end;
+  end;
+  
+  writeln(K)
+      
+end.
---
+++ 29_01/z3.pas
@@ -0,0 +1,27 @@
+﻿// Дано целое число N (> 1) и набор из N целых чисел. Вывести те эле-
+// менты в наборе, которые меньше своего левого соседа, и количество K та-
+// ких элементов.
+
+var
+  N, number, K, leftNeighbour: integer;
+  
+begin
+  readln(N);
+  K := 0;
+  for var i := 1 to N do 
+  begin
+    readln(number);
+    if (i <> 1) and (number < leftNeighbour) then
+      begin
+        K += 1;
+        writeln(number);
+      end;
+      
+      
+    leftNeighbour := number;
+    
+  end;
+  writeln(K)
+  
+    
+end.
