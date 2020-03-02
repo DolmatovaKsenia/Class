45275ed0ba2b54d9c298f755c2efa2f4b3128c45
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-03-02 20:13:46 +0300

    added classwork

---
+++ февраль/26.02.20/Z1.pas
@@ -0,0 +1,23 @@
+﻿// Series18. Дано целое число N и набор из N целых чисел, упорядоченный по
+// возрастанию. Данный набор может содержать одинаковые элементы. Вы-
+// вести в том же порядке все различные элементы данного набора.
+
+var
+  N, number, previousNumber: integer;
+  
+begin
+  readln(N);
+  readln(number);
+  previousNumber := number;
+  writeln(number);4
+  
+  for var i := 2 to N do
+  begin
+    readln(number);
+    if previousNumber < number then
+    begin
+      write(number, ' ');
+      previousNumber := number;
+    end;
+  end;
+end.
---
+++ февраль/26.02.20/Z2.pas
@@ -0,0 +1,34 @@
+﻿// Дано целое число N (> 2) и набор из N вещественных чисел. Набор
+// называется пилообразным, если каждый его внутренний элемент либо
+// больше, либо меньше обоих своих соседей (то есть является «зубцом»).
+// Если данный набор является пилообразным, то вывести 0; в противном
+// случае вывести номер первого элемента, не являющегося зубцом.
+
+var
+  N, number, previousNumber, numberBeforePrevious, badIndex, count: integer;
+  expression: boolean;
+  
+begin
+  expression := true;
+  readln(N);
+  readln(numberBeforePrevious);
+  readln(previousNumber);
+  count := 3;
+  for var i := 3 to N do
+  begin
+    readln(number);
+    if not ((number > numberBeforePrevious) and (number > previousNumber) or (number < numberBeforePrevious) and (number < previousNumber)) and (expression) then
+    begin
+      expression := false;
+      badIndex := count;
+    end;
+    count += 1;
+   numberBeforePrevious := previousNumber;
+   previousNumber := number;
+      
+  end;
+  if expression then
+    writeln('0')
+  else
+    writeln(badIndex)
+end.
