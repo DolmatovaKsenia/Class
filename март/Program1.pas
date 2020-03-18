7a1897a36c812c0581ba93dcffea4355165b2377
Victoria <viky.fomina@yandex.ru>
2020-03-18 19:50:08 +0300

    added example

---
+++ март/Program1.pas
@@ -0,0 +1,26 @@
+﻿const
+  rows = 4;
+  columns = 5;
+
+var
+  arr: array[1..rows, 1..columns] of integer;
+  
+begin
+// бежим по строкам, заполняем массив
+  for var i := 1 to rows do
+  begin
+    for var j := 1 to columns do
+    begin
+      arr[i, j] := random(0, 9);
+    end;  
+  end;
+  
+  for var i := 1 to rows do
+  begin
+    for var j := 1 to columns do
+    begin
+      write(arr[i, j], ' ');
+    end;
+    writeln();
+  end;
+end.
