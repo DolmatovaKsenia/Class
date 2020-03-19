93dd3010597e07f4faa04bedd03ae12a381fcb6e
DolmatovaKsenia <fantastic.drogon@gmail.com>
2020-03-19 21:06:24 +0300

    added classwork

---
+++ март/18.03.20/Z1.pas
@@ -0,0 +1,21 @@
+﻿const
+  rows = 5;
+  columns = 5;
+
+var
+  arr: array[1..rows,1..columns] of integer;
+  
+begin
+  for var i := 1 to rows do
+  begin
+    for var j := 1 to columns do
+      arr[i,j] := i * 10;
+  end;
+  
+  for var i := 1 to rows do
+  begin
+    for var j := 1 to columns do
+      write(arr[i,j],' ');
+    writeln()
+  end;
+end.
---
+++ март/18.03.20/Z2.pas
@@ -0,0 +1,23 @@
+﻿const
+  rows = 5;
+  columns = 4;
+  
+var 
+  arr: array[1..rows,1..columns] of integer;
+  
+begin
+  for var j := 1 to columns do
+  begin
+    for var i := 1 to rows do
+    begin
+      arr[i,j] := j * 5;
+    end;
+  end;
+  
+  for var i := 1 to rows do
+  begin
+    for var j := 1 to columns do
+      write(arr[i,j],' ');
+    writeln()
+  end;
+end.
