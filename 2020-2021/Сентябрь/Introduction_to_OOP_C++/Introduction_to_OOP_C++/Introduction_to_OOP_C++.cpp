66f85521f92fb81599606854b9d65ef9e1c63eef
Victoria <viky.fomina@yandex.ru>
2020-09-20 23:14:20 +0300

    added classwork

---
+++ 2020-2021/Сентябрь/Introduction_to_OOP_C++/Introduction_to_OOP_C++/Introduction_to_OOP_C++.cpp
@@ -0,0 +1,38 @@
+﻿
+
+#include <iostream>
+#include "Product.h"
+#include <cassert>
+
+// Параметры по умолчанию
+/*
+class Point
+{
+    int x = 5;
+};
+*/
+
+using namespace std;
+
+int main()
+{
+    // Контроль некорректного ввода.
+    
+    int positiveNumber = 0;
+    cout << "Input positive number: ";
+    cin >> positiveNumber;
+
+    // Варианты развития событий:
+    // - упасть 
+    // assert(positiveNumber > 0);
+    // - попросить ввести число еще раз (до тех пор, пока пользователь не введет положительное число)
+
+    while (positiveNumber <= 0)
+    {
+        cout << "Incorrect input!!!" << endl;
+        cout << "Input positive number: ";
+        cin >> positiveNumber;
+    }
+
+    return 0;
+}
---
+++ 2020-2021/Сентябрь/Pointers/Pointers/Pointers.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void basicPointers();
+
+void swap(int* a, int* b);
+
+int main()
+{    
+
+    return 0;
+}
+
+void workWithSwap()
+{
+    int a = 2;
+    int b = 5;
+    cout << "a = " << a << "; b = " << b << endl;
+
+    swap(&a, &b);
+    cout << "a = " << a << "; b = " << b << endl;
+}
+
+void swap(int* a, int* b)
+{
+    int temp = *a;
+
+    // *а - меняем значение по адресу на который указывает указатель (a)
+    // *b - получаем значение по адресу на который указывает указатель (b)
+    *a = *b;
+    *b = temp;
+}
+
+void basicPointers()
+{
+    int a = 3;
+    cout << "a = " << a << endl;
+
+    int* p; // указатель на тип int
+    p = &a; // взятие адреса
+
+
+    // * - разыменование укзателя (получение значения)
+    *p = 5; // меняем значение по адресу на который указывает указатель
+
+    cout << "a = " << a << endl;
+}
